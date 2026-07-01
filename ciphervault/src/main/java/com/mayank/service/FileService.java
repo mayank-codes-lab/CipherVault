177203b88c24fa3ea4ef33c25e70baaf08f235fb
Mayank Kumar <mayankvermav22@gmail.com>
2026-07-01 13:47:15 +0530

    Add file extention recognization feature in encryption module

--- ciphervault/src/main/java/com/mayank/service/FileService.java
+++ ciphervault/src/main/java/com/mayank/service/FileService.java
@@ -1,69 +1,66 @@
 package com.mayank.service;
+
 import java.io.File;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileService {
     private final Scanner scanner = new Scanner(System.in);
-    public void fileEncryption(){
+// try{
+    public void fileEncryption() {
         System.out.println("Enter File Path ");
         String filePath = scanner.nextLine();
         File file = new File(filePath);
-        if(filePath.isEmpty()){
+        String filename = file.getName();
+        if (filePath.trim().isEmpty()) {
             System.out.println("Path Should Not be Empty!");
             System.out.println("Enter Valid File Path!");
-        
-        }
-        else if(file.exists()){
-            System.out.println("File Foundat: " + filePath);
-            if(file.isFile()){
+
+        } 
+        else {
+            if (file.exists()) {
+                System.out.println("File Foundat: " + filePath);
+                if (file.isFile()) {
                     System.out.println("Its an File");
-                    if(file.canRead()){
+                    if (file.canRead()) {
                         System.out.println("File is Readable");
-                        
-                    }
-                    else{
+                        if (filename.endsWith(".txt")){
+                            System.out.println("its an Text file");
+                        }
+                        else if(filename.endsWith(".docx")){
+                            System.out.println("Its an Document file");
+                        }
+                        else{
+                            System.out.println("The file format is not supported");
+                            System.out.println("Only .txt and .docx files are supported");
+                        }
+                    } else {
                         System.out.println("File is not Readable");
                     }
-                   
-          }else{
-            System.out.println("Is not an file!");
-            
-          }
-           
+
+                } else {
+                    System.out.println("Is not an file!");
+
+                }
+
+            } else {
+                System.out.println("File not Found!");
+            }
         }
-        else{
-            System.out.println("File not Found!");
+        try{
+       FileReader fileReader = new FileReader(file);
+      
+       fileReader.close();
         }
-        // else if(file.isFile()){
-        //     System.out.println("its an File");
-          
-        // }
-        // else if(file.canRead()){
-        //     System.out.println("Files are Readable!");
-        // }
-        // else{
-        //     System.out.println("Files Not Found!");
-        // }
-
-        // File file = new File(filePath);
-        // if(filePath.isEmpty()){
-        //     System.out.println("Path should not be Empty! ");
-        //     System.out.println("Plz Enter the File Path");
-        //     return;
-        // }
-        // else if(file.exists()){
-        //     System.out.println("File Found!");
-            
-        // }
-        // else if(file.isFile()){
-
-        // }
-        // else{
-        //     System.out.println("File Not Found");
-        // }
-
-        
-
-
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
+
 }
+
+
+
+
